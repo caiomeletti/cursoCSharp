65e3b277774d9d64c85c5025c46f455562e7ebc4
Caio R. Meletti <caio@cliqx.com.br>
2023-05-17 19:48:47 -0300

    Adição de validações

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -2,10 +2,42 @@
 {
     public static void Main(string[] args)
     {
-        const int ANO_ATUAL = 2023;
-        Console.WriteLine("Qual o seu ano de nascimento?");
-        int anoNasc = int.Parse(Console.ReadLine());
-        int idade = ANO_ATUAL - anoNasc;
+        int ANO_ATUAL = DateTime.Now.Year;
+        int anoMin = ANO_ATUAL - 150;
+        int anoN = 0;
+        string? anoNasc = "";
+        bool parseOK = false;
+        bool anoMinOk = false;
+        bool anoMaxOk = false;
+        do
+        {
+            Console.WriteLine("Qual o seu ano de nascimento?");
+            anoNasc = Console.ReadLine();
+
+            if (int.TryParse(anoNasc, out anoN))
+                parseOK = true;
+            else
+                parseOK = false;
+
+            if (anoN < anoMin)
+            {
+                Console.WriteLine($"O ano deve ser maior que {anoMin}");
+                anoMinOk = false;
+            }
+            else
+                anoMinOk = true;
+
+            if (anoN > ANO_ATUAL)
+            {
+                Console.WriteLine($"O ano deve ser menor que {ANO_ATUAL}");
+                anoMaxOk = false;
+            }
+            else
+                anoMaxOk = true;
+
+        } while (!parseOK || !anoMinOk || !anoMaxOk);
+
+        int idade = ANO_ATUAL - anoN;
         Console.WriteLine($"Você tem {idade} anos de idade.");
     }
 }
